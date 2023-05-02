8b198accabbe0bc0bfd49fc795e6415cd2ef3289
manojnani166 <116333393+manojnani166@users.noreply.github.com>
2023-05-02 11:40:08 +0530

    first commit

---
+++ src/constants.jsx
@@ -0,0 +1,94 @@
+export const jsQuizz = {
+  questions: [
+  {
+  question:
+  "Which of the following is used in React.js to increase performance?",
+  choices: [
+  "Virtual DOM",
+  "Original DOM",
+  "Both A and B",
+  "None of the above",
+  ],
+  type: "MCQs",
+  correctAnswer: "Virtual DOM",
+  },
+  {
+  question: "What is ReactJS?",
+  choices: [
+  "Server-side framework",
+  "User Interface framework",
+  "Both A and B",
+  "None of the above",
+  ],
+  type: "MCQs",
+  correctAnswer: "User Interface framework",
+  },
+  {
+  question:
+  "Identify the one which is used to pass data to components from outside",
+  choices: ["Render with arguments", "setState", "PropTypes", "props"],
+  type: "MCQs",
+  correctAnswer: "props",
+  },
+  {
+  question: "In which language is React.js written?",
+  choices: ["Python", "Java", "C#", "JavaScript"],
+  type: "MCQs",
+  correctAnswer: "JavaScript",
+  },
+  {
+  question: "What is Babel?",
+  choices: [
+  "JavaScript interpreter",
+  "JavaScript transpiler",
+  "JavaScript compiler",
+  "None of the above",
+  ],
+  type: "MCQs",
+  correctAnswer: "JavaScript compiler",
+  },
+  {
+  question:
+  "What is a pure function in JavaScript?",
+  choices: [
+  "A function that returns the same output for the same input, and does not modify any external state",
+  "A function that returns a promise",
+  "A function that uses a callback",
+  "A function that can only be called once",
+  ],
+  type: "MCQs",
+  correctAnswer: "A function that returns the same output for the same input, and does not modify any external state",
+  },
+  {
+  question:
+  "What is the difference between synchronous and asynchronous programming in JavaScript?",
+  choices: [
+  "Synchronous programming blocks the thread until a task is completed, while asynchronous programming allows the thread to continue executing while a task is being completed",
+  "Asynchronous programming blocks the thread until a task is completed, while synchronous programming allows the thread to continue executing while a task is being completed",
+  "Synchronous and asynchronous programming are interchangeable",
+  "None of the above",
+  ],
+  type: "MCQs",
+  correctAnswer: "Synchronous programming blocks the thread until a task is completed, while asynchronous programming allows the thread to continue executing while a task is being completed",
+  },
+  {
+  question:
+  "What is a higher-order function in JavaScript?",
+  choices: [
+  "A function that takes another function as an argument, or returns a function as its result",
+  "A function that can be called only once",
+  "A function that returns a promise",
+  "A function that can be called with any number of arguments",
+  ],
+  type: "MCQs",
+  correctAnswer: "A function that takes another function as an argument, or returns a function as its result",
+  },
+ ],
+};
+
+export const resultInitalState = {
+  score: 0,
+  correctAnswers: 0,
+  wrongAnswers: 0,
+};
+
